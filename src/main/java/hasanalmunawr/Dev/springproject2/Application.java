942a053d9b0b82d59d8b1d14d62ff75f7ca215af
hasanalmunawr <hasanalmunawar01@gmail.com>
2024-04-29 10:37:29 +0700

    change a little point

--- src/main/java/hasanalmunawr/Dev/springproject2/Application.java
+++ src/main/java/hasanalmunawr/Dev/springproject2/Application.java
@@ -1,13 +1,45 @@
 package hasanalmunawr.Dev.springproject2;
 
+import hasanalmunawr.Dev.springproject2.domain.RequestContext;
+import hasanalmunawr.Dev.springproject2.entity.RoleEntity;
+import hasanalmunawr.Dev.springproject2.enumeration.Authority;
+import hasanalmunawr.Dev.springproject2.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.info.ProjectInfoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
 public class Application {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(RoleRepository roleRepository, ProjectInfoAutoConfiguration projectInfoAutoConfiguration) {
+		return args -> {
+
+			RequestContext.setUserId(0L);
+			RoleEntity userRole = new RoleEntity();
+			userRole.setName(Authority.USER.getName());
+			userRole.setAuthorities(Authority.USER);
+//			roleRepository.save(userRole);
+
+			var adminRole = new RoleEntity();
+			adminRole.setName(Authority.ADMIN.getName());
+			adminRole.setAuthorities(Authority.ADMIN);
+//			roleRepository.save(adminRole);
+
+			RequestContext.start();
+		};
+	}
 }
